a992ef44398e6ba0dfed29c4a7bf2a0110ba6edf
Emmy Whitford <emmyw@live.unc.edu>
2021-11-29 22:34:06 -0500

    every even attempt

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -94,8 +94,19 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
+    for (let i=0; i<arr.length; i++) {
+        if (i % 2 ==0) {
+            if (!test(arr[i])){
+                return false;
+            } else {
+                continue;
+            }
+        }
+    }
+    return true;
+}
+    
 
-};
 
 
 /**
