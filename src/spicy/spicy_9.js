0b4b42e08fc70049ad75e97ecad79149d11ced2c
Emmy Whitford <emmyw@live.unc.edu>
2021-11-29 23:58:30 -0500

    some even attempt

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -129,8 +129,17 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
-};
+    for (let i=0; i<arr.length; i++) {
+        if (i % 2 ==0) {
+            if (!test(arr[i])){
+                continue;
+            } else {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 
 /**
